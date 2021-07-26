016025a811f2ab5677231249f3e2eadef7dfd186
manhndhe141634 <80667923+manhndhth2000@users.noreply.github.com>
2021-07-26 15:01:26 +0700

    Update sql

--- Project_PRJ321/Project-Prj321.sql
+++ Project_PRJ321/Project-Prj321.sql
@@ -1,7 +1,13 @@
+USE [master]
+GO
+
+/****** Object:  Database [Project_Prj321]    Script Date: 7/26/2021 2:53:50 PM ******/
+CREATE DATABASE [Project_Prj321]
+
 USE [Project_Prj321]
 GO
 
-/****** Object:  Table [dbo].[tblAdmin]    Script Date: 6/25/2021 5:34:00 PM ******/
+/****** Object:  Table [dbo].[tblAdmin]    Script Date: 7/26/2021 2:54:49 PM ******/
 SET ANSI_NULLS ON
 GO
 
@@ -13,11 +19,15 @@ CREATE TABLE [dbo].[tblAdmin](
 	[username] [varchar](50) NOT NULL,
 	[password] [varchar](50) NOT NULL,
 	[name] [varchar](50) NOT NULL,
+	[dob] [date] NULL,
+	[information] [text] NOT NULL,
+	[email] [nvarchar](50) NOT NULL,
+	[image] [nvarchar](50) NULL,
  CONSTRAINT [PK_tblAdmin] PRIMARY KEY CLUSTERED 
 (
 	[id] ASC
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
-) ON [PRIMARY]
+) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
 GO
 
 CREATE TABLE [dbo].[tblBlogPost](
@@ -26,8 +36,8 @@ CREATE TABLE [dbo].[tblBlogPost](
 	[author_id] [int] NOT NULL,
 	[title] [varchar](100) NOT NULL,
 	[description] [text] NOT NULL,
-	[image] [image] NULL,
-	[timePost] [date] NOT NULL,
+	[image] [nvarchar](50) NULL,
+	[timePost] [datetime2](0) NOT NULL,
 	[shortDes] [text] NOT NULL,
  CONSTRAINT [PK_tblBlogPost] PRIMARY KEY CLUSTERED 
 (
@@ -36,26 +46,57 @@ CREATE TABLE [dbo].[tblBlogPost](
 ) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
 GO
 
+ALTER TABLE [dbo].[tblBlogPost]  WITH CHECK ADD  CONSTRAINT [FK_tblBlogPost_tblAdmin] FOREIGN KEY([author_id])
+REFERENCES [dbo].[tblAdmin] ([id])
+GO
+
+ALTER TABLE [dbo].[tblBlogPost] CHECK CONSTRAINT [FK_tblBlogPost_tblAdmin]
+GO
+
+ALTER TABLE [dbo].[tblBlogPost]  WITH CHECK ADD  CONSTRAINT [FK_tblBlogPost_tblCategory] FOREIGN KEY([category_id])
+REFERENCES [dbo].[tblCategory] ([id])
+GO
+
+ALTER TABLE [dbo].[tblBlogPost] CHECK CONSTRAINT [FK_tblBlogPost_tblCategory]
+GO
+
 CREATE TABLE [dbo].[tblCategory](
 	[id] [int] IDENTITY(1,1) NOT NULL,
 	[name] [varchar](50) NOT NULL,
+	[shortDescription] [text] NOT NULL,
  CONSTRAINT [PK_tblCategory] PRIMARY KEY CLUSTERED 
 (
 	[id] ASC
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
-) ON [PRIMARY]
+) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
 GO
 
 CREATE TABLE [dbo].[tblComment](
 	[id] [int] IDENTITY(1,1) NOT NULL,
 	[user_id] [int] NOT NULL,
 	[blog_post_id] [int] NOT NULL,
+	[timePost] [datetime2](0) NOT NULL,
 	[description] [text] NOT NULL,
+	[like] [int] NULL,
  CONSTRAINT [PK_tblComment] PRIMARY KEY CLUSTERED 
 (
 	[id] ASC
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
 ) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
+GO
+
+ALTER TABLE [dbo].[tblComment]  WITH CHECK ADD  CONSTRAINT [FK_tblComment_tblBlogPost] FOREIGN KEY([blog_post_id])
+REFERENCES [dbo].[tblBlogPost] ([id])
+GO
+
+ALTER TABLE [dbo].[tblComment] CHECK CONSTRAINT [FK_tblComment_tblBlogPost]
+GO
+
+ALTER TABLE [dbo].[tblComment]  WITH CHECK ADD  CONSTRAINT [FK_tblComment_tblUser] FOREIGN KEY([user_id])
+REFERENCES [dbo].[tblUser] ([id])
+GO
+
+ALTER TABLE [dbo].[tblComment] CHECK CONSTRAINT [FK_tblComment_tblUser]
 GO
 
 CREATE TABLE [dbo].[tblUser](
@@ -64,13 +105,34 @@ CREATE TABLE [dbo].[tblUser](
 	[password] [varchar](50) NOT NULL,
 	[email] [varchar](50) NOT NULL,
 	[name] [varchar](50) NOT NULL,
-	[gender] [bit] NULL,
+	[gender] [nvarchar](50) NULL,
 	[address] [varchar](100) NULL,
 	[dateofbirth] [date] NULL,
+	[image] [nvarchar](50) NULL,
  CONSTRAINT [PK_tblUser] PRIMARY KEY CLUSTERED 
 (
 	[id] ASC
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
 ) ON [PRIMARY]
+GO
+
+CREATE TABLE [dbo].[LikeAndUnlike](
+	[cmtId] [int] NOT NULL,
+	[user] [int] NULL
+) ON [PRIMARY]
+GO
+
+ALTER TABLE [dbo].[LikeAndUnlike]  WITH CHECK ADD  CONSTRAINT [FK_LikeAndUnlike_tblComment] FOREIGN KEY([cmtId])
+REFERENCES [dbo].[tblComment] ([id])
+GO
+
+ALTER TABLE [dbo].[LikeAndUnlike] CHECK CONSTRAINT [FK_LikeAndUnlike_tblComment]
+GO
+
+ALTER TABLE [dbo].[LikeAndUnlike]  WITH CHECK ADD  CONSTRAINT [FK_LikeAndUnlike_tblUser] FOREIGN KEY([user])
+REFERENCES [dbo].[tblUser] ([id])
+GO
+
+ALTER TABLE [dbo].[LikeAndUnlike] CHECK CONSTRAINT [FK_LikeAndUnlike_tblUser]
 GO
 
